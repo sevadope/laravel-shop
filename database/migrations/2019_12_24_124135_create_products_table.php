8362c2e91c5fb44d75477c3f4d046e23da564d0d
Kaizzzoku <ka1zzzoku@gmail.com>
2019-12-24 19:36:02 +0300

    Create model and migration for products

---
+++ database/migrations/2019_12_24_124135_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->jsonb('options');
+
+            $table->timestamps();
+        });
+
+        DB::connection()->getPdo()->exec('ALTER TABLE products ADD COLUMN price money;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
