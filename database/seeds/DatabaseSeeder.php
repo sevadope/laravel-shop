8c3f32352b545f9cdb7a770ce8d2e6530eb41636
Kaizzzoku <ka1zzzoku@gmail.com>
2020-01-17 17:07:05 +0300

    Create seeders for user roles and permissions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {   
+        $this->call(AuthSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductsOptionSeeder::class);
         $this->call(ProductsOptionValueSeeder::class);
